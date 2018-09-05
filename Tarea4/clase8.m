27edf082f9974445cf4334b92c30fd291d788aae
VICTOR DANIEL CRUZ GONZALEZ <vcruzgon@itam.mx>
2018-09-05 10:35:20 -0500

    Clase de precision numerica

---
+++ Tarea4/clase8.m
@@ -0,0 +1,18 @@
+% Obtener el epsilon de la máquina
+x = 1;
+t = 1;
+k = 0;
+xa = 0; % x anterior
+
+while (x ~= xa)
+    k = k + 1;
+    t = t / 2;
+    xa = x;
+    x = x + t;
+    t
+    k
+end
+
+log2(t)
+
+%% IEEE-754
