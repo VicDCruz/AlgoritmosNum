67a2dfefe4e2a5156e6dd2c3a31c1024921ec865
hca <hca@itam.mx>
2018-09-24 10:27:52 -0500

    Clase de interpolación

---
+++ Tarea5/interpLagrange.m
@@ -0,0 +1,18 @@
+function yLag = interpLagrange(xi, yi, xVal)
+
+    n= length(xi);
+    if length(yi) ~=n
+        error('Los vectores X y Y, no tienen la misma longitud');
+    end
+    suma = 0;
+    for i = 1:n
+        producto = yi(i);
+        for j = 1:n
+            if j ~= i
+                producto = producto * (xVal - xi(j))/(xi(i) - xi(j));
+            end            
+        end
+        suma = suma + producto;
+    end
+    yLag = suma;
+end
